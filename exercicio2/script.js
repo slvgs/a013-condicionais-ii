e77f908b96c1bc340ffe9bf16bbdf1a7d9de4437
slvgs <gabriellasilveriosantana15@gmail.com>
2022-09-08 15:59:06 -0300

    Concluídos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,6 +1,6 @@
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
+/*if(nacionalidade === "brasileira"){
     console.log("a pessoa é do Brasil!");
 } else if(nacionalidade === "argentina"){
     console.log("a pessoa é da Argentina!");
@@ -12,4 +12,26 @@ if(nacionalidade === "brasileira"){
     console.log("a pessoa é da Colômbia!");
 } else{
     console.log("nacionalidade não encontrada")
+}*/
+
+switch (nacionalidade) {
+    case "brasileira":
+        console.log("A pessoa é do Brasil!")
+        break;
+    case "argentina":
+        console.log("A pessoa é da Argentina!")
+        break;
+    case "uruguaia":
+        console.log("A pessoa é do Uruguai!")
+        break;
+    case "chilena":
+        console.log("A pessoa é do Chile!")
+        break;
+    case "colombiana":
+        console.log("A pessoa é da Colombia")
+        break
+
+    default:
+        console.log("Nacionalidade não encontrada")
+        break;
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,8 @@
 //código a ser reescrito
+nome = prompt("Qual seu nome?")
+idade = Number(prompt("Qual sua idade?"))
 
-if(nome === "José"){
+/*if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
 	console.log("Olá, "+nome);
@@ -10,4 +12,9 @@ if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
-}
+}*/
+
+
+
+nome === "José" ? console.log("Oi,Zé") : console.log("Olá, "+ nome)
+idade ===  18 || idade > 18 ? console.log("pode tirar carteira de motorista") : console.log("Ainda não pode tirar a carteira de motorista!")
